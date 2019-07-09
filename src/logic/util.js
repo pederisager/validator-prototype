a4eb61758c7418f3590dd3525a77b318f29dadc2
Felix Henninger <mailbox@felixhenninger.com>
2019-07-09 19:45:41 +0200

    Handle common path prefixes
    (these can occur if users upload an entire directory, in which case the directory name is prepended to all file names)

--- src/logic/util.js
+++ src/logic/util.js
@@ -1,2 +1,27 @@
 export const getFile = (path, files, prefix='') =>
   files.filter(f => f.path === prefix + path)[0]
+
+const trimLeadingSlash = path =>
+  path.startsWith('/')
+    ? path.slice(1)
+    : path
+
+// Detect the common path prefix given an array of files
+// TODO: This would probably work better with a Trie
+// or a similar data structure
+export const detectPrefix = (files) => {
+  // Split paths along slashes
+  const stems = new Set(files.map(
+    f => trimLeadingSlash(f.path).split('/')[0]
+  ))
+
+  // A stem is detected if there is one
+  // and only one common initial path
+  return stems.size === 1
+    ? '/' + stems.values().next().value
+    : ''
+    // TODO: The code above is somewhat ugly,
+    // because the set contents is only available
+    // through an iterator.
+    // Maybe there's a nicer solution?
+}
